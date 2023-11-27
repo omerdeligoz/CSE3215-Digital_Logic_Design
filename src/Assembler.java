fa25f231081af518d4fcdee5a2112b2f76a6203e
omerdeligoz <deligozomer@hotmail.com>
2023-11-27 22:21:16 +0300

    "Implemented binary code assembly process for AssemblerV2.java"
    A new version of the Assembler.java file, AssemblerV2.java, was created. This version includes the process to convert program instructions into binary format for further processing. It reads instructions from an input.txt file, interprets them to binary code, and provides exception handling for invalid commands or arguments.
    
    The assembly process features opcodes and registers mapping along with binary formatting according to the instruction type. The error messages offer insights into incorrect instructions such as out-of-range values or improper format.
    
    This addition aims to improve efficiency and provide a more automated and reliable assembly procedure for binary code generation.

--- src/Assembler.java
+++ src/Assembler.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Assembler {
-    final static Map<String,String> OPCODES = new HashMap<>();
-    final static Map<String,String> REGISTERS = new HashMap<>();
+    final static Map<String, String> OPCODES = new HashMap<>();
+    final static Map<String, String> REGISTERS = new HashMap<>();
 
     static {
         OPCODES.put("ADD", "0000");
@@ -39,7 +42,80 @@ public class Assembler {
         REGISTERS.put("R14", "1110");
         REGISTERS.put("R15", "1111");
     }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        File file = new File("input.txt");
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.printf("%s\n", line);
+                line = line.replaceAll(",", "");
+                String[] tokens = line.split(" ");
+                String opcode = OPCODES.get(tokens[0]);
+                String instruction = tokens[0];
+                String REG, DR, SR, SR1, SR2, OP1, OP2, IMM, ADDR, binaryADDR;
+
+
+                switch (instruction) {
+                    case "ADD":
+                    case "AND":
+                    case "NAND":
+                    case "NOR":
+                        DR = tokens[1];
+                        SR1 = tokens[2];
+                        SR2 = tokens[3];
+                        System.out.printf("%s %s %s %s 00\n", opcode, REGISTERS.get(DR), REGISTERS.get(SR1), REGISTERS.get(SR2));
+                        break;
+                    case "ADDI":
+                    case "ANDI":
+                        DR = tokens[1];
+                        SR = tokens[2];
+                        IMM = tokens[3];
+                        String binaryIMM = Integer.toBinaryString(Integer.parseInt(IMM));
+                        if (Integer.parseInt(IMM) < -32 || Integer.parseInt(IMM) > 31) {
+                            throw new IllegalArgumentException("Immediate value out of range!");
+                        }
+                        if (Integer.parseInt(IMM) < 0) {
+                            binaryIMM = binaryIMM.substring(binaryIMM.length() - 6);
+                        }
+                        binaryIMM = String.format("%06d", Integer.parseInt(binaryIMM));
+
+                        System.out.printf("%s %s %s %s\n", opcode, REGISTERS.get(DR), REGISTERS.get(SR), binaryIMM);
+                        break;
+                    case "LD":
+                    case "ST":
+                        REG = tokens[1];
+                        ADDR = tokens[2];
+                        binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
+                        if (Integer.parseInt(ADDR) > 1023) {
+                            throw new IllegalArgumentException("Address out of range!");
+                        }
+                        binaryADDR = String.format("%010d", Integer.parseInt(binaryADDR));
+                        System.out.printf("%s %s %s\n", opcode, REGISTERS.get(REG), binaryADDR);
+                        break;
+                    case "CMP":
+                        OP1 = tokens[1];
+                        OP2 = tokens[2];
+                        System.out.printf("%s %s %s 000000\n", opcode, REGISTERS.get(OP1), REGISTERS.get(OP2));
+                        break;
+                    case "JUMP":
+                    case "JE":
+                    case "JA":
+                    case "JB":
+                    case "JAE":
+                    case "JBE":
+                        ADDR = tokens[1];
+                        binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
+                        if (Integer.parseInt(ADDR) > 1023) {
+                            throw new IllegalArgumentException("Address out of range!");
+                        }
+                        binaryADDR = String.format("%010d", Integer.parseInt(binaryADDR));
+                        System.out.printf("%s 0000 %s\n", opcode, binaryADDR);
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
