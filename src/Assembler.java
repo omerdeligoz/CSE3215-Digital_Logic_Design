2f34bb4386ae2ccac557704b63d861e3e14c939d
omerdeligoz <deligozomer@hotmail.com>
2023-12-12 22:00:18 +0300

    12.12.2023

--- src/Assembler.java
+++ src/Assembler.java
@@ -1,29 +1,28 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Assembler {
-    final static Map<String, String> OPCODES = new HashMap<>();
-    final static Map<String, String> REGISTERS = new HashMap<>();
+    static final Map<String, String> OPCODES = new HashMap<>();
+    static final Map<String, String> REGISTERS = new HashMap<>();
+    static String output = "v2.0 raw\n";
 
     static {
-        OPCODES.put("ADD", "0000");
-        OPCODES.put("ADDI", "0001");
-        OPCODES.put("AND", "0010");
-        OPCODES.put("ANDI", "0011");
-        OPCODES.put("NAND", "0100");
-        OPCODES.put("NOR", "0101");
-        OPCODES.put("LD", "0110");
-        OPCODES.put("ST", "0111");
-        OPCODES.put("CMP", "1000");
-        OPCODES.put("JUMP", "1001");
-        OPCODES.put("JE", "1010");
-        OPCODES.put("JA", "1011");
-        OPCODES.put("JB", "1100");
-        OPCODES.put("JAE", "1101");
-        OPCODES.put("JBE", "1110");
+        OPCODES.put("ADD", "000");
+        OPCODES.put("ADDI", "000");
+        OPCODES.put("AND", "001");
+        OPCODES.put("ANDI", "001");
+        OPCODES.put("NAND", "010");
+        OPCODES.put("NOR", "011");
+        OPCODES.put("LD", "100");
+        OPCODES.put("ST", "101");
+        OPCODES.put("CMP", "110");
+        OPCODES.put("JUMP", "111");
+        OPCODES.put("JE", "111");
+        OPCODES.put("JA", "111");
+        OPCODES.put("JB", "111");
+        OPCODES.put("JAE", "111");
+        OPCODES.put("JBE", "111");
 
         REGISTERS.put("R0", "0000");
         REGISTERS.put("R1", "0001");
@@ -44,78 +43,150 @@ public class Assembler {
     }
 
     public static void main(String[] args) {
+        readInput();
+        writeOutput();
+    }
+
+    public static void writeOutput() {
+        try {
+            File file = new File("output");
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fw);
+            writer.write(output);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void readInput() {
         File file = new File("input");
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
+            // Read each line from the input file
             while ((line = br.readLine()) != null) {
-                System.out.printf("%s\n", line);
-                line = line.replaceAll(",", "");
+                // Remove commas and split the line into tokens
+                line = line.replace(",", "");
                 String[] tokens = line.split(" ");
-                String opcode = OPCODES.get(tokens[0]);
-                String instruction = tokens[0];
-                String REG, DR, SR, SR1, SR2, OP1, OP2, IMM, ADDR, binaryADDR;
-
-
-                switch (instruction) {
-                    case "ADD":
-                    case "AND":
-                    case "NAND":
-                    case "NOR":
-                        DR = tokens[1];
-                        SR1 = tokens[2];
-                        SR2 = tokens[3];
-                        System.out.printf("%s %s %s %s 00\n", opcode, REGISTERS.get(DR), REGISTERS.get(SR1), REGISTERS.get(SR2));
-                        break;
-                    case "ADDI":
-                    case "ANDI":
-                        DR = tokens[1];
-                        SR = tokens[2];
-                        IMM = tokens[3];
-                        String binaryIMM = Integer.toBinaryString(Integer.parseInt(IMM));
-                        if (Integer.parseInt(IMM) < -32 || Integer.parseInt(IMM) > 31) {
-                            throw new IllegalArgumentException("Immediate value out of range!");
-                        }
-                        if (Integer.parseInt(IMM) < 0) {
-                            binaryIMM = binaryIMM.substring(binaryIMM.length() - 6);
-                        }
-                        binaryIMM = String.format("%06d", Integer.parseInt(binaryIMM));
-
-                        System.out.printf("%s %s %s %s\n", opcode, REGISTERS.get(DR), REGISTERS.get(SR), binaryIMM);
-                        break;
-                    case "LD":
-                    case "ST":
-                        REG = tokens[1];
-                        ADDR = tokens[2];
-                        binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
-                        if (Integer.parseInt(ADDR) > 1023) {
-                            throw new IllegalArgumentException("Address out of range!");
-                        }
-                        binaryADDR = String.format("%010d", Integer.parseInt(binaryADDR));
-                        System.out.printf("%s %s %s\n", opcode, REGISTERS.get(REG), binaryADDR);
-                        break;
-                    case "CMP":
-                        OP1 = tokens[1];
-                        OP2 = tokens[2];
-                        System.out.printf("%s %s %s 000000\n", opcode, REGISTERS.get(OP1), REGISTERS.get(OP2));
-                        break;
-                    case "JUMP":
-                    case "JE":
-                    case "JA":
-                    case "JB":
-                    case "JAE":
-                    case "JBE":
-                        ADDR = tokens[1];
-                        binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
-                        if (Integer.parseInt(ADDR) > 1023) {
-                            throw new IllegalArgumentException("Address out of range!");
-                        }
-                        binaryADDR = String.format("%010d", Integer.parseInt(binaryADDR));
-                        System.out.printf("%s 0000 %s\n", opcode, binaryADDR);
-                        break;
-                }
+                convertInstruction(tokens);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            System.exit(1);
         }
+    }
+
+    public static void convertInstruction(String[] tokens) {
+        // Get the opcode and instruction from the first token
+        String opcode = OPCODES.get(tokens[0]);
+        String instruction = tokens[0];
+        // Declare variables for the different parts of the instruction
+        String REG, DR, SR, SR1, SR2, OP1, OP2, IMM, ADDR, binaryADDR, binaryInstruction;
+        // Process the instruction based on its type
+        switch (instruction) {
+            case "ADD", "AND", "NAND", "NOR" -> {
+                // For these instructions, get the destination register and two source registers
+                DR = tokens[1];
+                SR1 = tokens[2];
+                SR2 = tokens[3];
+
+                // Convert the instruction to binary and add it to the output
+                binaryInstruction = opcode + REGISTERS.get(DR) + REGISTERS.get(SR1) + "000" + REGISTERS.get(SR2);
+                convertAndAddToOutput(binaryInstruction);
+            }
+            case "ADDI", "ANDI" -> {
+                // For these instructions, get the destination register, source register and immediate value
+                DR = tokens[1];
+                SR = tokens[2];
+                IMM = tokens[3];
+                String binaryIMM = Integer.toBinaryString(Integer.parseInt(IMM));
+                if (Integer.parseInt(IMM) < -32 || Integer.parseInt(IMM) > 31) {
+                    throw new IllegalArgumentException("Immediate value out of range!");
+                }
+                if (Integer.parseInt(IMM) < 0) {
+                    binaryIMM = binaryIMM.substring(binaryIMM.length() - 6);
+                } else {
+                    binaryIMM = String.format("%06d", Integer.parseInt(binaryIMM));
+                }
+                binaryInstruction = opcode + REGISTERS.get(DR) + REGISTERS.get(SR) + "1" + binaryIMM;
+                convertAndAddToOutput(binaryInstruction);
+            }
+            case "LD", "ST" -> {
+                // For these instructions, get the register and address
+                REG = tokens[1];
+                ADDR = tokens[2];
+                binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
+                if (Integer.parseInt(ADDR) > 2047) {
+                    throw new IllegalArgumentException("Address out of range!");
+                }
+                binaryADDR = String.format("%011d", Integer.parseInt(binaryADDR));
+                binaryInstruction = opcode + REGISTERS.get(REG) + binaryADDR;
+                convertAndAddToOutput(binaryInstruction);
+            }
+            case "CMP" -> {
+                // For this instruction, get the two operands
+                OP1 = tokens[1];
+                OP2 = tokens[2];
+                binaryInstruction = opcode + REGISTERS.get(OP1) + REGISTERS.get(OP2) + "0000000";
+                convertAndAddToOutput(binaryInstruction);
+            }
+            case "JUMP", "JE", "JA", "JB", "JAE", "JBE" -> {
+                // For these instructions, get the address
+                ADDR = tokens[1];
+                binaryADDR = Integer.toBinaryString(Integer.parseInt(ADDR));
+                if (Integer.parseInt(ADDR) > 2047) {
+                    throw new IllegalArgumentException("Address out of range!");
+                }
+                binaryADDR = String.format("%011d", Integer.parseInt(binaryADDR));
+                switch (instruction) {
+                    case "JUMP" -> {
+                        binaryInstruction = opcode + "0000" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                    case "JE" -> {
+                        binaryInstruction = opcode + "0001" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                    case "JA" -> {
+                        binaryInstruction = opcode + "0010" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                    case "JB" -> {
+                        binaryInstruction = opcode + "0011" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                    case "JAE" -> {
+                        binaryInstruction = opcode + "0100" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                    case "JBE" -> {
+                        binaryInstruction = opcode + "0101" + binaryADDR;
+                        convertAndAddToOutput(binaryInstruction);
+                    }
+                }
+            }
+            default -> throw new IllegalArgumentException("Invalid instruction: " + instruction);
+        }
+    }
+
+    public static void convertAndAddToOutput(String binaryInstruction) {
+        // Convert the binary instruction to hexadecimal and add it to the output
+        String hexInstruction = binaryToHex(binaryInstruction);
+        output += hexInstruction + " ";
+    }
+
+    public static String binaryToHex(String binaryInstruction) {
+        // Check if the binary input length is exactly 18 bits
+        if (binaryInstruction.length() != 18) {
+            throw new IllegalArgumentException("Input must be an 18-bit binary string");
+        }
+
+        int decimalValue = Integer.parseInt(binaryInstruction, 2);
+        StringBuilder resultHex = new StringBuilder(Integer.toHexString(decimalValue));
+        while (resultHex.length() < 5) {
+            resultHex.insert(0, "0");
+        }
+        return resultHex.toString();
     }
 }
